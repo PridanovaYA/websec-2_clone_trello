c8fca281449d1a7acfe4a55ee8cc7d0f4db71ff7
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2023-02-15 02:37:47 +0400

    implemented the addition of a board

--- websec-2_clone_trello/public/script.js
+++ websec-2_clone_trello/public/script.js
@@ -1,5 +1,6 @@
 
 const lists = document.querySelectorAll('.list');
+const button = document.querySelector('.button')
 
 function addTask() {
     const btn = document.querySelector('.add_btn');
@@ -47,3 +48,24 @@ function addTask() {
 }
 
 addTask();
+
+function addBoard() {
+    const boards = document.querySelector('.boards')
+    const board = document.createElement('div')
+    board.classList.add('boards__item')
+    board.innerHTML = `
+        <span contenteditable="true" class="title"></span>
+        <div class="list"></div>
+    `
+    boards.append(board)
+    changeTitle()
+}
+button.addEventListener('click', addBoard)
+
+function changeTitle() {
+    const titles = document.querySelectorAll('.title')
+    titles.forEach(title => {
+        title.addEventListener('click', e => e.target.textContent = '')
+    })
+}
+changeTitle();

3c09d8752dc89c654c12ad182cf41ada5ea6bc86
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2023-02-15 16:09:21 +0400

    drag and drop is implemented

--- websec-2_clone_trello/public/script.js
+++ websec-2_clone_trello/public/script.js
@@ -44,6 +44,7 @@ function addTask() {
         form.style.display = 'none'
         btn.style.display = 'flex'
 
+        DragAndDrop()
     })
 }
 
@@ -59,7 +60,12 @@ function addBoard() {
     `
     boards.append(board)
     changeTitle()
+    DragAndDrop()
+    board.addEventListener('dblclick', () => {
+        board.remove()
+    })
 }
+
 button.addEventListener('click', addBoard)
 
 function changeTitle() {
@@ -68,4 +74,57 @@ function changeTitle() {
         title.addEventListener('click', e => e.target.textContent = '')
     })
 }
+
 changeTitle();
+
+let dragItem = null;
+
+function DragAndDrop() {
+    const listItem = document.querySelectorAll('.list__item')
+    const lists = document.querySelectorAll('.list')
+
+    for (let i = 0; i < listItem.length; i++) {
+
+        const item = listItem[i]
+        item.addEventListener('dragstart', () => {
+            dragItem = item
+            setTimeout(() => {
+                item.style, display = 'none'
+            }, 0)
+        })
+
+        item.addEventListener('dragend', () => {
+            setTimeout(() => {
+                item.style, display = 'block'
+                dragItem = null
+            }, 0)
+        })
+
+        item.addEventListener('dblclick', () => {
+            item.remove()
+        })
+
+        for (let g = 0; g < lists.length; g++) {
+            const list = lists[g]
+            list.addEventListener('dragover', e => e.preventDefault())
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault()
+                this.style.backgroundColor = 'rgba(0,0,0,.2)'
+
+            })
+
+            list.addEventListener('dragleave', function (e) {
+                this.style.backgroundColor = 'rgba(0,0,0,.0)'
+            })
+
+            list.addEventListener('drop', function (e) {
+                this.style.backgroundColor = 'rgba(0,0,0,.0)'
+                this.append(dragItem)
+            })
+
+        }
+    }
+
+}
+
+DragAndDrop();
